72ca1b9119a595f4ee3e8d89a0abe98bf6e1b46e
Yamusa Dalhatu <Elyamus@Yamusas-MacBook-Pro.local>
2017-04-03 15:27:32 +0700

    Solved solution

--- CCTableViewController.h
+++ CCTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  CCTableViewController.h
 //  UITableViewController Challenge Solution
 //
 //  Created by Yamusa Dalhatu on 4/3/17.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface CCTableViewController : UITableViewController
 
 @end
-
