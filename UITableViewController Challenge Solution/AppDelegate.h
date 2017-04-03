c27834f6c6b85fde379a1abe064da826f8464a71
Yamusa Dalhatu <Elyamus@yamusas-macbook-pro.local>
2017-04-03 14:25:07 +0700

    Initial Commit

---
+++ UITableViewController Challenge Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Yamusa Dalhatu on 4/3/17.
+//  Copyright © 2017 Elyamus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewController Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Yamusa Dalhatu on 4/3/17.
+//  Copyright © 2017 Elyamus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
